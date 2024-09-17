1bb0b74ca314fe72ab87ff805f3eca16cf2f142b
zach <zach@zachs-MacBook-Pro.local>
2024-09-17 18:42:43 -0500

    collected data, completed report, organized folder

--- assignment1/plot_loss.py
+++ assignment1/plot_loss.py
@@ -3,9 +3,21 @@ import numpy as np
 import json
 
 models = ["relu_64_28_64", "relu_256_512_128", "sigmoid_64_128_64", "sigmoid_256_512_128"]
-for name in models:
+figure, axis = plt.subplots(2,2)
+axes = [[0,0],[0,1],[1,0],[1,1]]
+i = 0
+xlabel = "Training Time"
+ylabel = "Error"
+for name, ax in zip(models,axes):
     with open("history_"+name+".json", "r") as f:
         loss_data = json.loads(f.read())
-    plt.plot(range(len(loss_data['loss'])), loss_data['loss'], label=name)
-    plt.legend()
-plt.savefig('history_77_plot.png')
+    axis[ax[0]][ax[1]].plot(np.linspace(0,int(loss_data['training_time']), len(loss_data['mean_squared_error'])), loss_data['mean_squared_error'], label="Training Error")
+    axis[ax[0]][ax[1]].plot(np.linspace(0,int(loss_data['training_time']), len(loss_data['val_mean_squared_error'])), loss_data['val_mean_squared_error'], label="Validation Error")
+    #axis[ax[0]][ax[1]].plot(range(len(loss_data['val_loss'])), loss_data['val_loss'], label="Validation Loss")
+    axis[ax[0]][ax[1]].set_title(name)
+    axis[ax[0]][ax[1]].set_xlabel(xlabel + " (s)")
+    axis[ax[0]][ax[1]].set_ylabel(ylabel)
+figure.suptitle("Training Error vs Training Time")    
+plt.tight_layout()
+plt.legend()
+plt.savefig(f'{ylabel}_per_{xlabel}_plot_TaV.png')

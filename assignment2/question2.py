86c8957dc932561ecef30e36879742f926185014
zach <zach@zachs-MacBook-Pro.local>
2024-09-25 11:55:38 -0500

    switching to keras image preprocessing

--- assignment2/question2.py
+++ assignment2/question2.py
@@ -1,1 +1,7 @@
 import tensorflow as tf
+
+data =  tf.keras.preprocessing.image_dataset_from_directory(
+        directory="data/",
+        image_size=(220,220)
+        )
+print(data.element_spec[0].shape)

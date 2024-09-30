6317cef43c4b686bdfb3f2f5b5ead2337d943580
zpearson36 <dudefro3600@gmail.com>
2024-09-29 21:23:30 -0500

    Finished Assignment

--- assignment2/question1.py
+++ assignment2/question1.py
@@ -1,26 +1,36 @@
+import json
 import tensorflow as tf
+import time
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(32,5,(1,1),'same', activation="relu", input_shape=(32,32,3)),
+    tf.keras.layers.Conv2D(32,5,(3,3),'same', activation="relu", input_shape=(32,32,3)),
     tf.keras.layers.BatchNormalization(),
     tf.keras.layers.MaxPool2D((2,2)),
-    tf.keras.layers.Conv2D(64,3,(1,1),'same', activation="relu"),
+    tf.keras.layers.Conv2D(64,3,(3,3),'same', activation="relu"),
     tf.keras.layers.BatchNormalization(),
     tf.keras.layers.MaxPool2D((2,2)),
     tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(units=512,activation="relu"),
+    tf.keras.layers.Dense(units=512, activation="relu"),
     tf.keras.layers.Dense(units=10, activation="softmax")
     ])
 
+(xtrain, ytrain), (xtest, ytest) = tf.keras.datasets.cifar10.load_data()
+ytrain=tf.keras.utils.to_categorical(ytrain)
+ytest=tf.keras.utils.to_categorical(ytest)
+print(xtrain.shape)
+print(xtest.shape)
+print(ytest.shape)
+print(ytrain.shape)
+print(xtrain.shape[0]*.8*100)
 learning_rate = tf.keras.optimizers.schedules.CosineDecay(
     initial_learning_rate=0.1,
-    decay_steps=1000,
-    alpha=0.001
+    decay_steps=xtrain.shape[0]*.8*100,
+    alpha=0.01
 )
 
 optimizer = tf.keras.optimizers.SGD(
     learning_rate=learning_rate,
-    momentum=0.9
+    momentum=.1
 )
 
 model.compile(
@@ -29,13 +39,6 @@ model.compile(
     optimizer = optimizer
         )
 model.summary()
-(xtrain, ytrain), (xtest, ytest) = tf.keras.datasets.cifar10.load_data()
-ytrain=tf.keras.utils.to_categorical(ytrain)
-ytest=tf.keras.utils.to_categorical(ytest)
-print(xtrain.shape)
-print(xtest.shape)
-print(ytest.shape)
-print(ytrain.shape)
 results = model.evaluate(
         x=xtest,
         y=ytest,
@@ -44,15 +47,16 @@ results = model.evaluate(
         )
 
 print(results)
-
+start = time.time()
 history = model.fit(
             x=xtrain,
             y=ytrain,
             batch_size=32,
-            epochs=10,
+            epochs=100,
             validation_split=.2
           )
 print(history.history)
+history.history["training_time"] = time.time() - start
 with open("history.json", "w") as f:
     json.dump(history.history, f)
 results = model.evaluate(
--- assignment2/question2.py
+++ assignment2/question2.py
@@ -1,66 +1,89 @@
+import json
 import tensorflow as tf
+import time
 
-train, test =  tf.keras.preprocessing.image_dataset_from_directory(
-        directory="data/",
+train, validation = tf.keras.preprocessing.image_dataset_from_directory(
+        directory="data/train/",
         label_mode="categorical",
         image_size=(220,220),
         color_mode="grayscale",
-        crop_to_aspect_ratio=True,
-        validation_split=.2,
+        validation_split=.1,
         subset="both",
-        shuffle=False,
+        seed=1,
+        batch_size=32,
         )
-#data_train, labels_train = train.element_spec
-#data_val, labels_val =  val.element_spec
-#print(list(train))
+test = tf.keras.preprocessing.image_dataset_from_directory(
+        directory="data/test/",
+        label_mode="categorical",
+        image_size=(220,220),
+        color_mode="grayscale",
+        )
+
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(32,5,(1,1),'same', activation="relu", input_shape=(220,220,1)),
+    tf.keras.layers.RandomFlip("horizontal"),
+    tf.keras.layers.RandomRotation(.15),
+    tf.keras.layers.Conv2D(32,(3,3),padding='same', activation="elu", input_shape=(220,220,1), kernel_initializer='he_normal'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Conv2D(32,(3,3),padding='same', activation="elu", input_shape=(220,220,1), kernel_initializer='he_normal'),
     tf.keras.layers.BatchNormalization(),
     tf.keras.layers.MaxPool2D((2,2)),
-    tf.keras.layers.Conv2D(64,3,(1,1),'same', activation="relu"),
+    tf.keras.layers.Dropout(.2),
+
+    tf.keras.layers.Conv2D(64,(3,3),padding='same', activation="elu", kernel_initializer='he_normal'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Conv2D(64,(3,3),padding='same', activation="elu", kernel_initializer='he_normal'),
     tf.keras.layers.BatchNormalization(),
     tf.keras.layers.MaxPool2D((2,2)),
+    tf.keras.layers.Dropout(.3),
+
+    tf.keras.layers.Conv2D(128,(3,3),padding='same', activation="elu", kernel_initializer='he_normal'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Conv2D(128,(3,3),padding='same', activation="elu", kernel_initializer='he_normal'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.MaxPool2D((2,2)),
+    tf.keras.layers.Dropout(.4),
     tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(units=512,activation="relu"),
     tf.keras.layers.Dense(units=15, activation="softmax")
     ])
 
 learning_rate = tf.keras.optimizers.schedules.CosineDecay(
-    initial_learning_rate=0.1,
-    decay_steps=3,
-    alpha=0.001
+    initial_learning_rate=0.001,
+    decay_steps=500,
+    alpha=0.1
 )
 
-optimizer = tf.keras.optimizers.SGD(
-    learning_rate=learning_rate,
-    momentum=0.9
-)
+optimizer = tf.keras.optimizers.SGD(lr=0.0001, decay=1e-5)
+
+#optimizer = tf.keras.optimizers.RMSprop(lr=0.0001,decay=1e-5)
+
+#optimizer = tf.keras.optimizers.Adam(lr=0.0001, decay=1e-5)
 
 model.compile(
-        loss = tf.keras.losses.CategoricalCrossentropy(),
-        metrics = [tf.keras.metrics.MeanSquaredError(name="mean_squared_error", dtype=None), "accuracy"],
-    optimizer = optimizer
+        loss = 'categorical_crossentropy',
+        metrics = ["accuracy"],
+        optimizer = optimizer
         )
-model.summary()
 
 results = model.evaluate(
         test,
-        batch_size=32,
         return_dict=True
         )
 
 print(results)
-
+start = time.time()
 history = model.fit(
             train,
-            batch_size=32,
-            epochs=10,
+            validation_data=validation,
+            epochs=200,
           )
-print(history.history)
+history.history['training_time'] = time.time() - start
+with open('question_2_arch1_SGD_history.json', 'w') as f:
+    json.dump(history.history, f)
 results = model.evaluate(
         test,
-        batch_size=32,
         return_dict=True
         )
 
 print(results)
+with open('question_2_arch1_SGD_results.json', 'w') as f:
+    json.dump(results, f)

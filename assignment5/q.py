b9af9db9fdbe43559b1f72355aef9a2d6d860824
zpearson36 <dudefro3600@gmail.com>
2024-11-17 19:41:21 -0600

    implemented q-table cart-pole agent

--- assignment5/q.py
+++ assignment5/q.py
@@ -1,1 +1,36 @@
-import gymnasium
+from collections import defaultdict
+import gymnasium as gym
+import numpy as np
+import time
+
+e = gym.make('CartPole-v1')
+q = defaultdict(lambda: np.zeros(e.action_space.n))
+#print(np.random.uniform(low = 0, high = 1, size=([1,2,3,4,5])))
+#exit()
+obs, info = e.reset()
+eps = 1
+decay = .001
+t = tuple
+def t(n):
+    d = np.sign(n[2:])
+    return tuple(np.round(n[1:], 1))
+print("training...")
+for _ in range(1000):
+    if np.random.random() < eps: action = e.action_space.sample()
+    else: action = int(np.argmax(q[t(obs)]))
+    f_obs, reward, term, trunc, info = e.step(action)
+    f_q = (not term) * np.max(q[t(f_obs)])
+    t_d = (reward + .2 * f_q - q[t(obs)][action])
+    q[t(obs)][action] += .001 * t_d
+    if term: e.reset()
+    if eps > .1: eps -= decay
+    obs = f_obs
+print("training complete")
+e.close()
+e = gym.make('CartPole-v1', render_mode="human")
+obs, info = e.reset()
+for _ in range(1000):
+    action = int(np.argmax(q[t(obs)]))
+    obs, reward, term, trunc, info  = e.step(action)
+    if term: e.reset()
+e.close()
